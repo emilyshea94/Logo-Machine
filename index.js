a767ea835d3563a856caec37b37f848c1b4ba036
emily shea <emilyshea947@gmail.com>
2024-03-08 11:57:23 -0500

    shapes.js

--- index.js
+++ index.js
@@ -1,49 +1,56 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Triangle, Circle, Square } = require('./lib/shapes');
+const { Triangle, Circle, Square } = require('./lib/shape');
 
-async function getUserInput() {
-  const prompts = [
-    {
-      type: 'input',
-      name: 'shape',
-      message: 'Enter shape (circle, triangle, square):',
+const prompts = [
+  {
+    type: 'input',
+    name: 'text',
+    message: 'Enter up to three characters for text:',
+    validate: (input) => {
+      return input.length <= 3 ? true : 'Please enter up to three characters.';
     },
-    {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'Enter shape color:',
-    },
-  ];
+  },
+  {
+    type: 'input',
+    name: 'textColor',
+    message: 'Enter a color:'
+  },
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'please select a shape:',
+    choices: ['triangle', 'circle', 'square'],
+  },
+  {
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Enter shape color:',
+  },
+];
 
-  return inquirer.prompt(prompts);
-}
+inquirer.prompt(prompts).then((userInput) => {
 
-async function generateSVGFile() {
-  const userInput = await getUserInput();
 
   let selectedShape;
   switch (userInput.shape.toLowerCase()) {
     case 'triangle':
-      selectedShape = new Triangle();
+      selectedShape = new Triangle(userInput.textColor, userInput.text, userInput.shapeColor);
       break;
     case 'circle':
-      selectedShape = new Circle();
+      selectedShape = new Circle(userInput.textColor, userInput.text, userInput.shapeColor);
       break;
     case 'square':
-      selectedShape = new Square();
+      selectedShape = new Square(userInput.textColor, userInput.text, userInput.shapeColor);
       break;
     default:
       console.error('Invalid shape selected');
       return;
   }
 
-  selectedShape.setColor(userInput.shapeColor);
 
   const svgContent = selectedShape.render();
 
   fs.writeFileSync('logo.svg', svgContent);
   console.log('Generated logo.svg');
-}
-
-generateSVGFile();
+})
